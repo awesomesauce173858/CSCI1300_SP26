cc1452cd69773b26db64a44e49bb1480312e5b72
Jalynn Nicoly <97772154+Jalynnn@users.noreply.github.com>
2026-02-04 08:31:33 -0700

    Comments

--- Week4/Checkpoint2/FileInputOutputExample.cpp
+++ Week4/Checkpoint2/FileInputOutputExample.cpp
@@ -16,15 +16,17 @@ int main() {
     // If an error is printed - make sure you file is in the same file location
     if (!inFile.is_open()) {
         cout << "Error opening file!" << endl;
-        return 1;
+        return 1; // True
     }
 
     // Read and display each line in the file
     string line;
-    while(getline(inFile, line)) {
+    while(getline(inFile, line)) { // getline(cin, line)
         // Delimiters: Not relevant to everyone's data
         cout << line << endl;
     }
 
     inFile.close();
+
+    return 0; // False
 }
